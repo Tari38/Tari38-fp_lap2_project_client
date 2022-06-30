4a5d409bd01708621faea6ef61062e13c8cf3c83
Ben <bentidball97@gmail.com>
2022-06-30 09:49:01 +0100

    js update

--- js/requests.js
+++ js/requests.js
@@ -3,6 +3,10 @@
 //
 
 //Requires values of register form and validates password matchcase and not null username, email and password
+
+
+const API_URL = require('./url');
+
 async function registerFormValidation({username, email, password}, passwordConfirm){
     if(username && email && password){
       if(password == passwordConfirm){
@@ -13,116 +17,157 @@ async function registerFormValidation({username, email, password}, passwordConfi
     }
 }
 
-const url = 'http://localhost:3000';
+const regForm = document.querySelector('#register-form');
+if(regForm != null){
+  regForm.addEventListener('submit', submitRegister);
+}
 
 //Post request for submitting user data
-async function submitRegister(){
+async function submitRegister(e){
+  e.preventDefault();
 
-  // TEST PAYLOAD //
-  const testPayload = {username: "user",  email: "user@gamil.com", password:"pass"};
-  const passwordConfirm = document.querySelector('#register-password-confirm').value;
-  // e.preventDefault();
+  //collect data from submission of form data
+  const data = Object.fromEntries(new FormData(e.target));
+  const payload = {username: data['register-username'],  email: data['register-email'], password: data['register-password']};
+  const passwordConfirm =  data['register-password-confirm'];
 
-  if(registerFormValidation(testPayload, passwordConfirm)){
-    try {
+  if(registerFormValidation(payload, passwordConfirm)){
+    console.log("register valid")
+    try { 
       const options = {
           method: 'POST',
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(testPayload)
+          body: JSON.stringify(payload)
       }
-      const response = await fetch(`${url}/users`, options);
+      const response = await fetch(`${API_URL}/users`, options);
       if(!response.ok) { 
         throw console.error("Invalid request data");
       }
     } catch (err) {
       console.warn(err);
     }
+  }else{
+    throw console.error("Passwords do not match");
   }
 }
 
+const logForm = document.querySelector('#register-form');
+if(logForm != null){
+  logForm.addEventListener('submit', submitLogin);
+}
+
+
+
 //Post request for submitting login user data
-async function submitLogin(){
-  // TEST PAYLOAD //
-  const testPayload = {username: "user", password:"pass"};
-
+async function submitLogin(e){
+  e.preventDefault();
   // check if token exists and is valid
-  if(tokenIsValid){
-
-  }else{
-    // login and recieve new token
-    try {
-      const options = {
-          method: 'POST',
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(testPayload)
-      }
-      const response = await fetch(`${url}/users`, options);
-      const data = await response.json()
-      if(response.ok){
-        console.log(data);
-        saveToken(data);
-      }else { 
-        throw console.error("Invalid request data");
-      }
-
-    } catch (err) {
-      console.warn(err);
-    }
+  const token = localStorage.getItem('token');
+  console.log(token);
+  
+  // login and recieve new token
+  const userdata = Object.fromEntries(new FormData(e.target));
+  const Payload = {username: userdata["login-username"], password: userdata["login-password"]};
+  
+  const options = {
+    method: 'POST',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(Payload)
   }
+  const response = await fetch(`${url}/login`, options);
+  const data = await response.json()
+  if(response.ok){
+    console.log(data);
+    saveToken(data);
+  }else { 
+    console.error("Invalid request data");
+  }
+
+
+  // if(token != null){
+  //   // show user logged in and or redirect to habits
+  //   console.log(token);
+  // }
+}
+
+const windowName = window.location.pathname;
+if(windowName == '/client/static/private/accountPage.html'){
+  getHabits();
 }
 
 //Get request to obtain users habits
 async function getHabits(){
 
   //check if user is currently has valid token
-  if(currentUser()){
-    try {
-      // TEST PAYLOAD //
-      //user_id should be taken from token in local storage
-      const user_id = 1;
+  try{
+    if(currentUser()){
 
+      const user_id = localStorage.getItem('user_id');
       const options = {
           method: 'GET',
           headers: { "Content-Type": "application/json" },
       }
-      const response = await fetch(`${url}/habits/users/${user_id}`, options);
-      if(!response.ok) { 
+
+      const response = await fetch(`${API_URL}/habits/users/${user_id}`, options);
+      const data = await response.json()
+      if(response.ok) { 
+        populateHabitList(data);
+      }else{
         throw console.error("Invalid request data");
       }
-    } catch (err) {
-      console.warn(err);
+    }else{
+      // console.error("User is not logged in");
     }
-  }else{
-    throw console.error("User is not logged in");
+    
+  }catch{
+    throw console.warn('Could not complete request');
   }
 }
 
-//Post request create new habit bound to user
-async function createHabit(){
-  // TEST PAYLOAD //
-  let userId;
-  const testPayload = {user_id: userId, title: "test habit", time: 1200, freq: 2, comment: "test comment"};
-  
-  //check if user is currently has valid token
-  if(currentUser()){
-    try {
-      userId = 1;
-      //get user id from token
-      // userId = localStorage.getItem(user_id);
+const createHabitForm = document.querySelector('#create-habit-form');
+if(createHabitForm != null){
+  createHabitForm.addEventListener('submit', createHabit);
+}
 
+//Post request create new habit bound to user
+async function createHabit(e){
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(e.target));
+
+  // //check if user is currently logged in and entry is valid
+  if(currentUser() && validateHabitCreation(data)){
+    try{
+      // const userId = localStorage.getItem('user_id');
+      const userId = 1; //TEST DATA
+      const Payload = {name: data['new-habit-title'], frequency: parseInt(data['new-habit-freq']), time: parseInt(data['new-habit-time']), _comment: data['new-habit-comment'], user_id: userId,};
+    
       const options = {
-          method: 'POST',
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(testPayload)
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(Payload)
       }
+    
       const response = await fetch(`${url}/habits/`, options);
       if(!response.ok) { 
-        throw console.error("Invalid request data");
+        console.error("Invalid request data");
       }
-    } catch (err) {
-      console.warn(err);
+
+    }catch{
+      throw console.error("Request could not be complete");
     }
+
   }else{
-    throw console.error("User is not logged in");
+    console.error("User is not logged in");
   }
+};
+       
+function validateHabitCreation(data){
+  //if any value is null, validation will return false else true
+  for (const [key, value] of Object.entries(data)) {
+    console.log(value);
+    if(value == null){
+      return false;
+    }
+  }
+  return true;
 }
